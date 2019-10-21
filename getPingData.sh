46986d4ba3096bccea31d453fa1bed2861e51f37
mcginnisalexander <mcginnisalexander@gmail.com>
2019-10-21 00:17:41 -0700

    Update getPingData.sh

--- getPingData.sh
+++ getPingData.sh
@@ -2,8 +2,8 @@
 cat temp.txt >> tcpdumplog.txt
 rm temp.txt
 touch temp.txt
-tcpdump 'icmp[icmptype] = icmp-echo' -n -c 1 -A >> temp.txt &
-TASK_PID1=$!
-sleep 10
-kill $TASK_PID1
-echo 'dead' >> temp.txt
+tcpdump 'icmp[icmptype] = icmp-echo' -n -c 1 -A >> temp.txt 2>&1 &
+# TASK_PID1=$!
+# sleep 10
+# kill $TASK_PID1
+# echo 'dead' >> temp.txt
